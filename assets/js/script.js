56a2fff5293a30a21f017d9f8edbfdb799c63d41
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-06-18 15:59:49 -0300

    add search full name or full id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,115 @@
-const divPokemon = document.querySelector('#pokemons');
-const cardPokemon = document.querySelector('#pokemon-link');
-const maximumAmount = 60;
-const urlBase = 'https://pokeapi.co/api/v2/';
-
-window.addEventListener("scroll", (event) => {
-  console.log("aaaa");
-});
-
-const fetchPokemon = (currentPokemon) => {
-  const promises = [];
-
-  for (let i = currentPokemon; i <= maximumAmount; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+class Filter {
+  constructor(minAmount, maximumAmount) {
+    this.minAmount = minAmount;
+    this.maximumAmount = maximumAmount;
+    this.number = null;
+    this.name = null;
   }
 
-  return Promise.all(promises).then((results) => {
-    return results.map((data) => ({
-      name: data.name,
-      id: data.id,
-      image: data['sprites']['front_default'],
-      type: data.types.map((type) => type.type.name).join(", "),
-    }));
-  });
-};
-
-async function renderPokemons(currentPokemon) {
-  const allPokemon = await fetchPokemon(currentPokemon);
-
-  if (allPokemon) {
-    allPokemon.forEach((data) => {
-      let cardClone = cardPokemon.cloneNode(true);
-
-      cardClone.href = `${urlBase}/pokemon/${data.name}`;
-      cardClone.querySelector("#pokemon-image").src = data.image;
-      cardClone.querySelector("#pokemon-name").innerHTML = data.name;
-      cardClone.querySelector("#pokemon-id").innerHTML = data.id;
-      cardClone.classList.add(data.type.split(',')[0]);
-      divPokemon.append(cardClone);
-    });
-
-    cardPokemon.remove();
+  clean() {
+    this.number = null;
+    this.name = null;
   }
 }
 
-renderPokemons(1);
+const divPokemon = document.querySelector('#pokemons');
+const cardPokemon = document.querySelector('#pokemon-link');
+const searchPokemon = document.querySelector("#search-pokemon");
+const pokeball = document.querySelector("#pokeball");
+const urlBase = 'https://pokeapi.co/api/v2';
+const filter = new Filter(1, 100);
+
+searchPokemon.addEventListener("keyup", () => {
+  const search = searchPokemon.value;
+
+  if (!search) {
+    filter.clean();
+    renderPokemons();
+    return;
+  }
+
+  const isNumber = isNumeric(search);
+
+  if (isNumber) {
+    filter.number = search;
+  }
+
+  if (!isNumber) {
+    filter.name = encodeURIComponent(search).toLowerCase();
+  }
+
+  renderPokemons(filter);
+})
+
+const isNumeric = (value) => {
+  return /^-?\d+$/.test(value);
+}
+
+const searchPokemonByFilter = (filter) => {
+  const url = `${urlBase}/pokemon/${filter}`;
+  const data = fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error("HTTP status " + response.status);
+    }
+    return response.json();
+  }).catch((err) => {
+    return null;
+  });
+  return data;
+}
+
+const createCardPokemon = (currentPokemon) => {
+  let cardClone = cardPokemon.cloneNode(true);
+  cardClone.style.display = "block";
+  cardClone.href = `${urlBase}/pokemon/${currentPokemon.name}`;
+  cardClone.querySelector("#pokemon-image").src = currentPokemon.image;
+  cardClone.querySelector("#pokemon-name").innerHTML = currentPokemon.name;
+  cardClone.querySelector("#pokemon-id").innerHTML = currentPokemon.id;
+  cardClone.classList.add(currentPokemon.type.split(',')[0]);
+  divPokemon.append(cardClone);
+}
+
+const fetchPokemon = () => {
+  const promises = [];
+  const isSearch = filter.number || filter.name;
+
+  if (isSearch) {
+    promises.push(searchPokemonByFilter(isSearch));
+  }
+
+  if (!isSearch) {
+    for (let i = filter.minAmount; i <= filter.maximumAmount; i++) {
+      promises.push(searchPokemonByFilter(i));
+    }
+  }
+
+  return Promise.all(promises).then((results) => {
+    return results.map((data) => {
+      if (data) {
+        return {
+          name: data.name,
+          id: data.id,
+          image: data['sprites']['front_default'],
+          type: data.types.map((type) => type.type.name).join(", "),
+        }
+      }
+    });
+  });
+};
+
+async function renderPokemons() {
+  const allPokemon = await fetchPokemon();
+  divPokemon.innerHTML = "";
+  pokeball.style.display = "none";
+
+  if (allPokemon) {
+    divPokemon.innerHTML = "";
+    allPokemon.forEach((data) => {
+      if(data){
+        createCardPokemon(data);
+      }
+    });
+  }
+}
+
+renderPokemons();
